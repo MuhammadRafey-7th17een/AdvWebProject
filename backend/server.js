18728aa859826aeb0e42b01d2c3a0ca16d86a7a3
MuhammadRafey-7th17een <mrafey205@gmail.com>
2026-05-24 15:58:11 +0500

    added connection code

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,16 @@
 import express from 'express'
 import { userRouter } from './routes/userRoutes.js';
 import { errorMiddleware } from './middleware/errormiddleware.js';
+import mongoose from 'mongoose';
+import 'dotenv/config'
 const app = express()
-const PORT = 8080
-
+const PORT = process.env.PORT
 app.use(express.json());
-app.use("/api",userRouter);
+app.use("/user",userRouter);
 app.use(errorMiddleware);
+const url = process.env.Mongo_url
+mongoose.connect(url).then(()=> console.log("Connection built with DB")).catch((err) => console.log("Db error "+err))
+
 app.listen(PORT,()=>{
     console.log("Server running at "+PORT)
 })
