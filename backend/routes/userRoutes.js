602de46fe976a354b725683de99a3dd17a18b033
MuhammadRafey-7th17een <mrafey205@gmail.com>
2026-06-13 17:02:53 +0500

    cp

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,11 +1,18 @@
 import { createUser, deleteUser, searchByEmail,searchByPhoneNumber, updateEmail } from '../Controller/userController.js';
 import express from 'express'
-
+import rateLimit from 'express-rate-limit'
 export const userRouter = express.Router()
 
+
+const limiter = rateLimit({
+    windowMs = 24 * 60 * 60 * 1000,
+    max:10,
+    standardHeaders = true,
+    legacyHeaders = false
+})
 userRouter.get("/searchE/:email",searchByEmail)
 userRouter.get("/searchP",searchByPhoneNumber)
-userRouter.post("/create",createUser)
+userRouter.post("/create",   createUser)
 userRouter.put("/update/:id/:email",updateEmail)
 userRouter.delete("/delete/:id",deleteUser)
 
--- backend/server.js
+++ backend/server.js
@@ -3,13 +3,30 @@ import { userRouter } from './routes/userRoutes.js';
 import { errorMiddleware } from './middleware/errormiddleware.js';
 import mongoose from 'mongoose';
 import 'dotenv/config'
+import { authMiddleware } from './middleware/authMiddleware.js';
 
 
 const app = express()
 const PORT = process.env.PORT
 app.use(express.json());
-app.use("/user",userRouter);
+
 app.use(errorMiddleware);
+app.use(authMiddleware)
+
+const limiter = rateLimit({
+    windowMs = 24 * 60 * 60 * 1000,
+    max:(req,res)=>{
+        if(req.user){
+            return Number.MAX_SAFE_INTEGER
+        }
+        else{
+            return 10
+        }
+    },
+    standardHeaders = true,
+    legacyHeaders = false
+})
+app.use("/user",userRouter);
 const url = process.env.Mongo_url
 mongoose.connect(url).then(()=> console.log("Connection built with DB")).catch((err) => console.log("Db error "+err))
 app.listen(PORT,()=>{
